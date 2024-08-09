2f06ad8d05da8635c44e04eb3ef13e8ebc0f442f
Platinum-Pluto <mediocre.judge2000@gmail.com>
2024-08-09 15:42:48 +0600

    Fixed Font Lusitana Issue

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,6 +2,6 @@ import {Inter, Lusitana} from 'next/font/google';
 
 
 export const inter = Inter({ subsets: ['latin'] });
-export const luci = Lusitana({ subsets: ['latin'], weight: '400' });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
 
 
